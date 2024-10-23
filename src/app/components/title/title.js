8901b1456ae137b0d4bde374b148df0ce6970aa7
Mariana Amina <mariana.dantasweb7@gmail.com>
2024-10-23 11:48:55 -0300

    feat(components): Change Title structure to make it able to receive different heading levels

--- src/app/components/title/title.js
+++ src/app/components/title/title.js
@@ -1,7 +1,15 @@
 import styles from './title.module.scss';
 
-export default function Title({children}) {
-    return (
-        <h1 className={styles["main-title"]}>{children}</h1>
-    )
+export default function Title({children, level = 1}) {
+    switch(level) {
+        case 1: return (
+            <h1 className={styles["main-title"]}>{children}</h1>
+        )
+        case 2: return (
+            <h2 className={styles["subtitle"]}>{children}</h2>
+        )
+        case 3: return (
+            <h3 className={styles["subtitle-h3"]}>{children}</h3>
+        )
+    }
 }
