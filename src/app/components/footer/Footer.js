66494f6bcb55ed9eafb7b435362b3892b72df395
Mariana Amina <mariana.dantasweb7@gmail.com>
2024-10-25 10:39:55 -0300

    feat(components): create footer component based on tests

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Footer () {
-    return 
+    return (
+        <footer>
+            <div>
+                <p>Não tem uma conta? Que tristeza!</p>
+            </div>
+            <div>
+                <Link href="terms-and-conditions">Termos e Condições</Link>
+                <Link href="support">Suporte</Link>
+                <Link href="faq">Dúvidas</Link>
+            </div>
+        </footer>
+    )
 }
