1b9fe5280c675dad953e9b8f9934a30f004e2ee6
Mariana Amina <mariana.dantasweb7@gmail.com>
2024-10-28 11:26:25 -0300

    test(components): add unit tests for button component

---
+++ src/app/components/button/__test__/Button.test.js
@@ -0,0 +1,22 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import {toBeInTheDocument} from '@testing-library/jest-dom'
+import Button from '../Button'
+
+describe("Button component Unit Test", () => {
+    beforeEach(()=> {
+        render(<Button />)
+    })
+    it("should render on the screen", () => {
+        const buttonElement = screen.getByText("Login", {selector: "button"})
+        expect(buttonElement).toBeInTheDocument();
+        expect(buttonElement).toBeVisible();
+    })
+
+    it("should appear loading icon and be disabled when clicked", () => {
+        const buttonElement = screen.getByText("Login", {selector: "button"})
+        fireEvent.click(buttonElement);
+
+        expect(buttonElement.disabled).toBe(true);
+        expect(buttonElement.textContent).not.toBe("Login");
+    })
+})
