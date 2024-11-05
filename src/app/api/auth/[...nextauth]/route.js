661cc9069f68ef049b34ef3dc10570979c2d0877
Mariana Amina <mariana.dantasweb7@gmail.com>
2024-11-04 22:32:55 -0300

    fix(auth): fix redirect URI (again)

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -25,7 +25,7 @@ export const authOptions = {
                     response_type: 'code',
                     client_id: process.env.SPOTIFY_ID,
                     scope: "user-top-read user-library-read",
-                    redirect_uri: "https://dunder-mifflin-playlists.vercel.app/api/auth/callback/spotify",
+                    redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/spotify`,
                     state: state
                 }
             }
